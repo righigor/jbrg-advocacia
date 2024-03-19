e727d58a4341893ca4c0c9cc4e5d4b29ce641b24
Igor Righi <cruigor@gmail.com>
2024-03-19 15:30:50 -0300

    correcoes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -14,10 +14,8 @@ function About() {
         <img src={ perfil } alt="" className={ style.perfil }/>
         <div className={ style.aboutContainer }>
           <h2>Sobre</h2>
-          {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In porro consequuntur eligendi, libero facere similique quasi doloribus corporis delectus nemo maiores ad assumenda quas autem non nobis velit. Dolore, asperiores.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus obcaecati modi voluptatem, quae ex cum adipisci numquam corrupti alias reprehenderit cumque voluptas quia inventore error ipsam quasi dignissimos perspiciatis doloribus!</p> */}
           <p>A JBRG ADVOCACIA é um empresa jurídica, presente no mercado há vinte e oito anos, oferecendo serviços de conteúdo jurídico com ética e responsabilidade, disponibilizando uma equipe técnica capaz, preservando de forma intransigente valores como ética, compromisso, honestidade e competência, visando oferecer segurança jurídica e uma defesa implacável em favor dos interesses do cliente.</p>
-          <p>Nossa principal característica é o foco no resultado ou solução, utilizando as melhores ferramentas, compatibilizando conciliação e mediação, quanto possível, com o exercício da advocacia de forma independente, embora leal para com o nosso cliente, exercendo a defesa com persistência e zelo, de forma artesanal e personalizada.</p>
+          <p>Nossa principal característica é o foco no resultado ou solução, utilizando as melhores ferramentas, compatibilizando conciliação e mediação, quando possível, com o exercício da advocacia de forma independente, embora leal para com o nosso cliente, exercendo a defesa com persistência e zelo, de forma artesanal e personalizada.</p>
           <img src={ logo } alt="" className={ style.logo } />
         </div>
       </div>
